ae96310c5624ae5723771a901e012c0d1b8c23dd
Tyler Hanavan <TylerHanavan@gmail.com>
2016-10-10 17:24:13 -0400

    Commit

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 $(window).ready(function() {
   var width = 0;
+  var wait = 0;
   setInterval(function() {
-    ++width;
-    if(width > 100) {
-      width -= 99;
+    if(wait == 0) {
+      ++width;
+      if(width > 100) {
+        width -= 99;
+        wait = 10;
+      }
+      $('#progress').css('width', width + '%');
+    } else {
+      wait--;
     }
-    $('#progress').css('width', width + '%');
   }, 100);
 });
