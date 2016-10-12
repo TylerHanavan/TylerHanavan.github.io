d707b4cd9dbfe5ac7263bfde2a6ded7039cc780f
Tyler Hanavan <TylerHanavan@gmail.com>
2016-10-12 16:24:17 -0400

    Commit

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-$(window).ready(function() {
-  var width = 0;
-  var wait = 0;
-  setInterval(function() {
-    if(wait == 0) {
-      ++width;
-      if(width > 100) {
-        width = 0;
-        wait = 1;
-      }
-      $('#progress').css('width', width + '%');
-    } else {
-      $('#progress-div').css('display', 'none');
-      $('#progress').css('display', 'none');
-      $('#status').html('<h2>Go fuck yourself</h2>');
-    }
-  }, 50);
-});
+
