1304009f359b335d678b3b7b09ced6464eb28114
Tyler Hanavan <TylerHanavan@gmail.com>
2016-11-09 16:33:21 -0500

    Misspelling of LimeRoute

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var lime = new Lime();
 
-lime.doRoute(new Route("data", "http://www.masspvp.com/test/request.php", s, a));
+lime.doRoute(new LimeRoute("data", "http://www.masspvp.com/test/request.php", s, a));
 
 var request = lime.doRequest("data", {});
 
