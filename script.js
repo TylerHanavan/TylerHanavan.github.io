bc19cb796f55d7f1d99a0e505b02afa830b3396c
Tyler Hanavan <TylerHanavan@gmail.com>
2016-11-09 16:44:07 -0500

    Another update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ lime.addRoute(new LimeRoute("data", "request.html", s, a));
 var request = lime.doRequest("data", {});
 
 function s(response) {
-  $("#test").val(response.data);
+  $("#test").text(response.data);
 }
 
 function a(a, b, c) {
